651b66444fa47da923eea98558cd55583aa90db9
kehtoo <63067333+kehtoo@users.noreply.github.com>
2021-12-05 19:51:16 +0300

    Add files via upload

---
+++ z5/z5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static double[,] umn(double[,] arr1, double[,] arr2, int a1, int b1, int b2)
+        {
+            double[,] Mass = new double[a1, a1];
+            double sum = 0;
+            for (int i = 0; i < a1; i++)
+            {
+                for (int j = 0; j < b2; j++)
+                {
+                    for (int r = 0; r < b1; r++)
+                    {
+                        sum = sum + arr1[i, r] * arr2[r, j];
+                    }
+                    Mass[i, j] = sum;
+                    sum = 0;
+                }
+            }
+            return Mass;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5, b = 5, c = 5;
+            double[,] mass1 = new double[a, b];
+            double[,] mass2 = new double[b, c];
+            Console.WriteLine("Arr1: ");
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    mass1[i, j] = i + 1 + j;
+                    Console.Write(mass1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nArr2: ");
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    mass2[i, j] = i + 1 + j;
+                    Console.Write(mass2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nFinal arr:");
+            double[,] Mass = umn(mass1, mass2, a, b, c);
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write(Mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ z6/z6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(sumIterative(array));
+            Console.WriteLine(sumRecursive(array));
+            Console.WriteLine(minRecursive(array, array.Length));
+            Console.WriteLine(minIterative(array));
+            Console.ReadKey();
+        }
+
+        static int sumIterative(int[] array)
+        {
+            int sum = default;
+            for (int i = 0; i < array.Length; i++) sum += array[i];
+            return sum;
+        }
+
+        static int sumRecursive(int[] array, int i = 0) => i == array.Length - 1 ? array[i] : array[i] + sumRecursive(array, ++i);
+        static int minRecursive(int[] a, int n)
+        {
+            if (n == 1)
+            {
+                return a[0];
+            }
+            var newAr = new int[n - 1];
+            Array.Copy(a, 1, newAr, 0, n - 1);
+            var min = Math.Min(a[0], a[1]);
+            newAr[0] = min;
+            return minRecursive(newAr, n - 1);
+        }
+        static int minIterative(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            return min;
+        }
+    }
+}

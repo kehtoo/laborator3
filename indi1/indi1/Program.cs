29958eb76643954d6f60f83e78b28209b820b02a
kehtoo <63067333+kehtoo@users.noreply.github.com>
2021-12-05 19:54:41 +0300

    Add files via upload

---
+++ indi1/indi1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int m = n / 2 + n % 2,
+                len = n,
+                count = 1;
+            Console.WriteLine("Матрица " + n + " на " + n);
+            for (int i = 0; i < m; i++)
+            {
+                // Верх
+                for (int j = 0; j < len; j++) matrix[i, i + j] = count++;
+                // Право
+                for (int j = 1; j < len; j++) matrix[i + j, n - i - 1] = count++;
+                len -= 2;
+                // Низ
+                for (int j = len; j >= 0; j--) matrix[n - i - 1, i + j] = count++;
+                // Лево
+                for (int j = len; j >= 1; j--) matrix[i + j, i] = count++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ indi2/indi2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = random.Next(-10, 10);
+            }
+            int count = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) > 5) { count++; }
+                }
+                //Console.WriteLine("Count:" + count);
+            }
+            Console.WriteLine("Ur arr:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine("\nCount:" + count);
+            Console.ReadKey();
+        }
+    }
+}

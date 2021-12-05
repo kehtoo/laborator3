d490b40a5933294a340742432406a1fb638e90c5
kehtoo <63067333+kehtoo@users.noreply.github.com>
2021-12-05 19:53:40 +0300

    Add files via upload

---
+++ z7/z7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите n-ый номер ряда Фибоначчи: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"" + n + "-oe число:" + Fibonachi(n));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ z8/z8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 5;
+            double[,] matrix = new double[n, n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next(-5, 5);
+                }
+            }
+            var det = DetRec(matrix);
+            Console.WriteLine(det);
+            Console.ReadKey();
+        }
+        private static double DetRec(double[,] matrix)
+        {
+            if (matrix.Length == 4)
+            {
+                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            }
+            if (matrix.Length == 1) { return matrix[0, 0]; }
+            double sign = 1, result = 0;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                double[,] minor = GetMinor(matrix, i);
+                result += sign * matrix[0, i] * DetRec(minor);
+                sign = -sign;
+            }
+            return result;
+        }
+        private static double[,] GetMinor(double[,] matrix, int n)
+        {
+            double[,] result = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0, col = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == n)
+                        continue;
+                    result[i - 1, col] = matrix[i, j];
+                    col++;
+                }
+            }
+            return result;
+        }
+    }
+}
